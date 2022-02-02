1f557c738c58e130d7de012ca1a05c5d592a29f6
DJHero2903 <67331181+DJHero2903@users.noreply.github.com>
2022-02-02 21:39:37 +0000

    Ensure commands are ran one after another

--- Install.sh
+++ Install.sh
@@ -2,24 +2,26 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Run this script as root!" ; exit 1 ; fi
 
 # Update mirrors and install the virtualbox-guest-utils package.
-sudo pacman -Sy --noconfirm virtualbox-guest-utils  &
+sudo pacman -Sy --noconfirm virtualbox-guest-utils
 
 # Enable the virtualbox service 
-sudo systemctl enable --now vboxservice.service &
+sudo systemctl enable --now vboxservice.service
 
 # Add the virtualbox modules to the Linux Kernel.
-sudo modprobe -a vboxguest vboxsf vboxvideo &
+sudo modprobe -a vboxguest vboxsf vboxvideo
 
 # Invalidate the cached sudo credentials.
 sudo -k
 
 # Enable Virtualbox client features.
-VBoxClient --clipboard &
-VBoxClient --draganddrop &
-VBoxClient --seamless &
-VBoxClient --checkhostversion &
-VBoxClient --vmsvga &
+VBoxClient --clipboard
+VBoxClient --draganddrop
+VBoxClient --seamless
+VBoxClient --checkhostversion
+VBoxClient --vmsvga
 
 # Exit script and show the guest additions were installed.
-echo "\n\nVirtual box guest additions installed!\nPlease reboot your system." &
+echo ""
+echo "\n\nVirtual box guest additions installed!\nPlease reboot your system."
+
 exit 1
