55c77612ab174075b82ff50c7487709b355be23f
DJHero2903 <67331181+DJHero2903@users.noreply.github.com>
2022-07-12 17:21:28 +0100

    fix: :bug: Only run required comamnds as root instead of the entire script.

--- Install.sh
+++ Install.sh
@@ -1,17 +1,21 @@
-# Check is the script is being run as root, if not exit.
-if [[ $(id -u) -ne 0 ]] ; then echo "Run this script as root!" ; exit 1 ; fi
+# Exit the script if it's being run as root.
+if [[ $(id -u) -eq 0 ]] ; then echo "Don't run this script as root!" ; exit 1 ; fi
 
 # Update mirrors and install the virtualbox-guest-utils package.
-sudo pacman -Sy --noconfirm virtualbox-guest-utils
+sudo pacman -Sy --noconfirm --needed virtualbox-guest-utils
+echo "virtualbox-guest-utils installed."
 
 # Enable the virtualbox service 
 sudo systemctl enable --now vboxservice.service
+echo "Virtualbox service enabled."
 
 # Add the virtualbox modules to the Linux Kernel.
 sudo modprobe -a vboxguest vboxsf vboxvideo
+echo "Virtualbox kernal modules installed."
 
-# Invalidate the cached sudo credentials.
-sudo -k
+# Add $USER to the vboxsf group.
+usermod -aG vboxsf $USER
+echo "$USER has been added to the vboxsf group."
 
 # Enable Virtualbox client features.
 VBoxClient --clipboard
@@ -19,9 +23,9 @@ VBoxClient --draganddrop
 VBoxClient --seamless
 VBoxClient --checkhostversion
 VBoxClient --vmsvga
+echo "clipboard, draganddrop, seamless, checkhostversion, vmsvga client features enabled."
 
 # Exit script and show the guest additions were installed.
-echo ""
 echo "Virtual box guest additions installed! Please reboot your system."
 
 exit 1
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Arch-Vbox-Guest-Utils-Install
+
 A script to install the virtual box guest additions on Arch linux based systems.
+
+## Installation Guide
+
+1. Clone the repository. `git clone https://github.com/DJHero2903/Arch-Vbox-Guest-Utils-Install.git`
+1. Enter the Arch-Vbox-Guest-Utils-Install directory. `cd Archive-Vbox-Guest-Utils-Install`
+1. Run the script as your normal user. `./Install.sh`
