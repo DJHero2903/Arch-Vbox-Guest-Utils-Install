243d7c3d5a20965f92403ddfb3346c3d23e94a70
DJHero2903 <67331181+DJHero2903@users.noreply.github.com>
2022-07-12 18:11:37 +0100

    style: :pencil2: Change yellow colour in echo output to light purple

--- Install.sh
+++ Install.sh
@@ -3,19 +3,19 @@ if [[ $(id -u) -eq 0 ]] ; then echo -e "\033[1;31mDon't run this script as root!
 
 # Update mirrors and install the virtualbox-guest-utils package.
 sudo pacman -Sy --noconfirm --needed virtualbox-guest-utils
-echo -e "\033[1;33mvirtualbox-guest-utils installed.\033[0m"
+echo -e "\033[1;35mvirtualbox-guest-utils installed.\033[0m"
 
 # Enable the virtualbox service 
 sudo systemctl enable --now vboxservice.service
-echo -e "\033[1;33mVirtualbox service enabled.\033[0m"
+echo -e "\033[1;35mVirtualbox service enabled.\033[0m"
 
 # Add the virtualbox modules to the Linux Kernel.
 sudo modprobe -a vboxguest vboxsf vboxvideo
-echo -e "\033[1;33mVirtualbox kernal modules installed.\033[0m"
+echo -e "\033[1;35mVirtualbox kernal modules installed.\033[0m"
 
 # Add the currently logged-in user to the vboxsf group.
 sudo usermod -aG vboxsf $LOGNAME
-echo -e "\033[1;36m$LOGNAME \033[1;33mhas been added to the vboxsf group.\033[0m"
+echo -e "\033[1;36m$LOGNAME \033[1;35mhas been added to the vboxsf group.\033[0m"
 
 # Enable Virtualbox client features.
 VBoxClient --clipboard
@@ -23,7 +23,7 @@ VBoxClient --draganddrop
 VBoxClient --seamless
 VBoxClient --checkhostversion
 VBoxClient --vmsvga
-echo -e "\033[1;33mclipboard, draganddrop, seamless, checkhostversion, vmsvga client features enabled.\033[0m"
+echo -e "\033[1;35mclipboard, draganddrop, seamless, checkhostversion, vmsvga client features enabled.\033[0m"
 
 # Exit script and show the guest additions were installed.
 echo -e "\n\033[1;32mVirtual box guest additions installed! Please reboot your system.\033[0m"
