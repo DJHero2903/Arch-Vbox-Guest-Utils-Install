ae44bf68d8fa281071d23f1056ecd6b593eebe87
DJHero2903 <67331181+DJHero2903@users.noreply.github.com>
2025-01-05 04:16:12 +0000

    use VBoxClient-all script

--- Install.sh
+++ Install.sh
@@ -6,26 +6,19 @@ sudo pacman -Sy --noconfirm --needed virtualbox-guest-utils
 echo -e "\033[1;35mvirtualbox-guest-utils installed.\033[0m"
 
 # Enable the virtualbox service 
-sudo systemctl enable --now vboxservice.service
+sudo systemctl enable vboxservice.service
 echo -e "\033[1;35mVirtualbox service enabled.\033[0m"
-
-# Add the virtualbox modules to the Linux Kernel.
-sudo modprobe -a vboxguest vboxsf vboxvideo
-echo -e "\033[1;35mVirtualbox kernal modules installed.\033[0m"
 
 # Add the currently logged-in user to the vboxsf group.
 sudo usermod -aG vboxsf $LOGNAME
 echo -e "\033[1;36m$LOGNAME \033[1;35mhas been added to the vboxsf group.\033[0m"
 
 # Enable Virtualbox client features.
-VBoxClient --clipboard
-VBoxClient --draganddrop
-VBoxClient --seamless
-VBoxClient --checkhostversion
-VBoxClient --vmsvga
-echo -e "\033[1;35mclipboard, draganddrop, seamless, checkhostversion, vmsvga client features enabled.\033[0m"
+VBoxClient-all
+
+echo -e "\033[1;35mRunning /usr/bin/VBoxClient-all script to enable Virtualbox client features\033[0m"
 
 # Exit script and show the guest additions were installed.
 echo -e "\n\033[1;32mVirtual box guest additions installed! Please reboot your system.\033[0m"
 
-exit 1
+exit 0
